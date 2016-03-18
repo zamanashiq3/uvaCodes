7585f1fbbbfebc45c66c7169b5998fc962999408
Ashiq Zaman <zamanashiq3@gmail.com>
2016-03-18 14:36:08 +0600

    18 March 2016 2:35 pm

--- Prob10464.java
+++ Prob10464.java
@@ -8,15 +8,26 @@ import java.math.BigDecimal;
 public class Prob10464 {
 
 	public static void main(String[] args) throws IOException{
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(reader.readLine().toString());
-		while (N--!=0) {
-			String[] numbers = reader.readLine().toString().split(" ");
-			BigDecimal a = new BigDecimal(numbers[0]);
-			BigDecimal b = new BigDecimal(numbers[1]);
-			System.out.println(a.add(b));
-		}
-
-	}
-
+        BufferedReader bufferReader=new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer stringBuffer = new StringBuffer("");
+        int cases=Integer.parseInt(bufferReader.readLine());
+        for(int i=0;i<cases;i++) {
+            String[] lines=bufferReader.readLine().split(" ");
+            BigDecimal a=new BigDecimal(lines[0]);
+            a=a.add(new BigDecimal(lines[1]));
+            StringBuilder temp=new StringBuilder(a.toString());
+            stringBuffer.append(resizingString(temp)).append("\n");
+        }
+        System.out.print(stringBuffer);
+    }
+     static String resizingString(StringBuilder s){
+         int index=s.length()-1;
+         while(s.charAt(index)=='0' && s.charAt(index-1)!='.'){
+             index--;
+         }
+        
+        return s.substring(0,index+1);
+    }
 }
+
+
